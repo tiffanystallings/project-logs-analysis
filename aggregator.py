bed4b33178dc024bc71284a57d496ca38d1cee6c
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-02 16:45:57 -0400

    Format code to match pep8 standards

--- aggregator.py
+++ aggregator.py
@@ -16,43 +16,46 @@ DBNAME = "news"
 db = psycopg2.connect(database=DBNAME)
 cursor = db.cursor()
 
-def top_posts(limit):
-	"""
-	Takes limit as input, where limit is an int
-	representing how many articles to print, 
-	and outputs a string containing that many
-	top articles and their page views, sorted
-	from most views to least.
-	"""
 
-	return limit
+def top_posts(limit):
+    """
+    Takes limit as input, where limit is an int
+    representing how many articles to print,
+    and outputs a string containing that many
+    top articles and their page views, sorted
+    from most views to least.
+    """
+
+    return limit
+
 
 def top_authors():
-	"""
-	Takes no inputs, outputs a string containing
-	all authors sorted and their page views,
-	sorted from most views to least.
-	"""
+    """
+    Takes no inputs, outputs a string containing
+    all authors sorted and their page views,
+    sorted from most views to least.
+    """
 
-	return 0
+    return 0
+
 
 def high_errors(percent):
-	"""
-	Takes percent as input, where percent is an int
-	representing the error occurance percentage
-	to check for, and outputs a string containing
-	all dates where errors occured over at a
-	higher percent than the input, and the percentage
-	of errors for that day.
-	"""
+    """
+    Takes percent as input, where percent is an int
+    representing the error occurance percentage
+    to check for, and outputs a string containing
+    all dates where errors occured over at a
+    higher percent than the input, and the percentage
+    of errors for that day.
+    """
 
-	return percent
+    return percent
 
 # Print results to console
-for result in [top_posts(3), 
-			   top_authors(), 
-			   high_errors(1)]:
-	print(result)
+for result in [top_posts(3),
+               top_authors(),
+               high_errors(1)]:
+    print(result)
 
 cursor.close()
 db.close()
